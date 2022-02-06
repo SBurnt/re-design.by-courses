42ae9d433437463fdf5f5bf9e1939b416cd2f5d0
SBurnt <zip2004zip@gmail.com>
2022-02-07 02:18:55 +0300

    add skills section

--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -1,25 +1,29 @@
+/* eslint-disable no-unused-vars */
 import Swiper from '../../../node_modules/swiper/swiper-bundle';
 
-const sliderList = document.querySelectorAll('.js-slider-init');
-if (sliderList) {
-	sliderList.forEach(el => {
-		const swiper = new Swiper(el, {
-			loop: true,
-			autoHeight: true,
-			// autoplay: {
-			// 	delay: 5000,
-			// 	disableOnInteraction: false,
-			// },
-			navigation: {
-				nextEl: '.slider__arrows-next',
-				prevEl: '.slider__arrows-prev',
-			},
-			breakpoints: {
-				// when window width is >= 768px
-				768: {
-					autoHeight: false,
-				},
-			},
-		});
+const sliderSkills = document.querySelector('.js-slider-skills-init');
+if (sliderSkills) {
+	const swiper = new Swiper(sliderSkills, {
+		loop: true,
+		autoHeight: true,
+		speed: 0,
+		// autoplay: {
+		// 	delay: 5000,
+		// 	disableOnInteraction: false,
+		// },
+		navigation: {
+			nextEl: '.slider__arrows-next',
+			prevEl: '.slider__arrows-prev',
+		},
+		pagination: {
+			el: '.slider__pagination',
+			clickable: true,
+		},
+		// breakpoints: {
+		// 	// when window width is >= 768px
+		// 	768: {
+		// 		autoHeight: false,
+		// 	},
+		// },
 	});
 }
