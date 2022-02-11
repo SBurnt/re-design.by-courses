e9034b1d95bf9f62aa6b002aa977f5e4cac1a1ea
SBurnt <zip2004zip@gmail.com>
2022-02-11 16:07:25 +0300

    add scroll to form

--- src/assets/files/mail.php
+++ src/assets/files/mail.php
@@ -3,9 +3,11 @@
 $username = $_POST['name'];
 $usertel = $_POST['tel'];
 $useremail = $_POST['email'];
+$typeCourse = $_POST['type'];
 
 $userdate = date('Y-m-d H:i:s');
 
 echo ' username ' . $username;
 echo ' usertel ' .  $usertel;
 echo ' useremail ' .  $useremail;
+echo ' typeCourse ' .  $typeCourse;
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,6 +5,7 @@ import './modals';
 import './swiper';
 import './validate-form';
 import './accordion';
+import './scrollToBlock';
 
 if (module.hot) {
 	module.hot.accept();
